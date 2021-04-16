22218d357ca767ebf849fc365f0a39eb11cd77e5
zpplzh <zappelzhu@gmail.com>
2021-04-16 15:40:58 +0700

    update sessionid length

--- internal/catalog/session.go
+++ internal/catalog/session.go
@@ -4,10 +4,10 @@ import "math/rand"
 
 func RandSessionid() string {
 
-	//var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-	var letterRunes = []rune("abcde")
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	//var letterRunes = []rune("abcde")
 
-	b := make([]rune, 2) //length of the random character is 35
+	b := make([]rune, 35) //length of the random character is 35
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
